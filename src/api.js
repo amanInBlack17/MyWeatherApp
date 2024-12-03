ec6aad3f87a06a19bac26b33d2dfaf959587802a
amanInBlack17 <amansunny2014@gmail.com>
2024-12-03 21:28:08 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions= {
+    method: "GET",
+    headers: {
+     "X-RapidAPI-Key": "792d9c691cmshde9c9b69ec73562p1ffefbjsnce6477637296",
+      "X-RapidAPI-Host":"wft-geo-db.p.rapidapi.com",
+    },
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "aac9797e6624b6077a39e3070d1af69d";
